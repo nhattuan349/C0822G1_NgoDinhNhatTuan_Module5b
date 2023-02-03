24b59fe4c5be10cb7644ad1e34724d2172a0e625
unknown <nhattuanqlmt2@gmail.com>
2023-02-03 13:29:24 +0700

    upadte

--- ss2-typescript/excercise/typescript-fibonacci/src/main.ts
+++ ss2-typescript/excercise/typescript-fibonacci/src/main.ts
@@ -1,15 +1,22 @@
 function findFibonacci(number:number):number{
+
     if (number <= 1){
         return number;
     }
     return findFibonacci(number-1) + findFibonacci(number-2);
 }
+
 let number: number = 10;
+
 let totalFibonacci: number = 0;
+
 let arrayFibonacci: Array<number> = [];
+
 for (let i = 0; i < number; i++) {
     arrayFibonacci.push(findFibonacci(i));
     totalFibonacci += findFibonacci(i);
 }
+
 console.log(arrayFibonacci);
+
 console.log(totalFibonacci);
